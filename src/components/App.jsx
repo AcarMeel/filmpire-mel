b5950bad5007d467352ba00911cef34ff1509833
acarmeel <acarmeel@MacBook-Pro-de-acarmeel.local>
2022-05-31 21:31:29 -0600

    Components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,32 @@
 import { CssBaseline } from '@mui/material';
-// import { Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom';
+import { MovieInformation, Actors,
+  Movies,
+  Profile,
+  NavBar } from './';
+
 
 // eslint-disable-next-line
 const App = () => (
   <div>
     <CssBaseline />
-    <h1>Hola</h1>
+    <NavBar />
+    <main>
+      <Switch>
+      <Route path={'/movie/:id'} exact>
+          <MovieInformation />
+        </Route>
+        <Route path={'/actors/:id'} exact>
+          <Actors />
+        </Route>
+        <Route path={'/'} exact>
+          <Movies />
+        </Route>
+        <Route path={'/profile/:id'} exact>
+          <Profile />
+        </Route>
+      </Switch>
+    </main>
   </div>
 );
 
