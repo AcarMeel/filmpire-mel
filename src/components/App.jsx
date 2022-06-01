ccc5eb21ef14f753137bc1c693e66c2f879be61f
acarmeel <acarmeel@MacBook-Pro-de-acarmeel.local>
2022-05-31 21:51:13 -0600

    Styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,33 +1,34 @@
-import { CssBaseline } from '@mui/material';
-import { Route, Switch } from 'react-router-dom';
-import { MovieInformation, Actors,
-  Movies,
-  Profile,
-  NavBar } from './';
-
+import { CssBaseline } from "@mui/material";
+import { Route, Switch } from "react-router-dom";
+import { MovieInformation, Actors, Movies, Profile, NavBar } from "./";
+import useStyles from "./styles";
 
 // eslint-disable-next-line
-const App = () => (
-  <div>
-    <CssBaseline />
-    <NavBar />
-    <main>
-      <Switch>
-      <Route path={'/movie/:id'} exact>
-          <MovieInformation />
-        </Route>
-        <Route path={'/actors/:id'} exact>
-          <Actors />
-        </Route>
-        <Route path={'/'} exact>
-          <Movies />
-        </Route>
-        <Route path={'/profile/:id'} exact>
-          <Profile />
-        </Route>
-      </Switch>
-    </main>
-  </div>
-);
+const App = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <CssBaseline />
+      <NavBar />
+      <main className={classes.content}>
+        <div className={classes.toolbar} />
+        <Switch>
+          <Route path={"/movie/:id"} exact>
+            <MovieInformation />
+          </Route>
+          <Route path={"/actors/:id"} exact>
+            <Actors />
+          </Route>
+          <Route path={"/"} exact>
+            <Movies />
+          </Route>
+          <Route path={"/profile/:id"} exact>
+            <Profile />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+};
 
 export default App;
