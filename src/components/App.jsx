290bf761e391ca1aa8bb14135d5acd1c0390a6a0
acarmeel <acarmeel@MacBook-Pro-de-acarmeel.local>
2022-05-31 20:45:19 -0600

    Downgrade versions

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,12 @@
-import React from 'react';
+import { CssBaseline } from '@mui/material';
+// import { Route, Switch } from 'react-router-dom'
 
+// eslint-disable-next-line
 const App = () => (
-  <div>App</div>
+  <div>
+    <CssBaseline />
+    <h1>Hola</h1>
+  </div>
 );
 
 export default App;
