f1e9170a2887e67f59389985d527a0e7f35dab1b
acarmeel <acarmeel@MacBook-Pro-de-acarmeel.local>
2022-06-05 21:54:42 -0600

    Fetch genres and add icons to sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -13,8 +13,16 @@ import {
   useTheme,
 } from "@mui/material";
 import { ClassNames } from "@emotion/react";
+import { useGetGenresQuery } from "../../services/TMDB";
+import genreIcons from '../../assets/genres';
 
 const Sidebar = ({ setMobileOpen }) => {
+  console.log(genreIcons)
+  const { data: genresList, isLoading, error } = useGetGenresQuery({
+    genreIdOrCategoryName: null,
+    page: 1,
+    searchQuery: null,
+  });
   const theme = useTheme();
   const classes = useStyles();
   const redLogo =
@@ -23,11 +31,6 @@ const Sidebar = ({ setMobileOpen }) => {
   const blueLogo =
     "https://fontmeme.com/permalink/210930/6854ae5c7f76597cf8680e48a2c8a50a.png";
 
-    const demoCategories = [
-        { label: 'Comedy', value: 'comedy' },
-        { label: 'Action', value: 'action' },
-        { label: 'Animation', value: 'animation' }
-    ];
     const categories = [
         { label: 'Popular', value: 'popular' },
         { label: 'Top Rated', value: 'top_rated' },
@@ -49,9 +52,9 @@ const Sidebar = ({ setMobileOpen }) => {
               categories.map(({label, value}) => (
                 <Link key={value} className={classes.links} to="/">
                     <ListItem onClick={() => {}} button>
-                        {/* <ListItemIcon>
-                            <img className={classes.genreImage} src={redLogo} alt="Logo" height={30} />
-                        </ListItemIcon> */}
+                        <ListItemIcon>
+                            <img className={classes.genreImage} src={genreIcons[label.toLowerCase()]} alt="Logo" height={30} />
+                        </ListItemIcon>
                         <ListItemText primary={label} />
                     </ListItem>
                 </Link>
@@ -62,13 +65,20 @@ const Sidebar = ({ setMobileOpen }) => {
       <List>
           <ListSubheader>Genres</ListSubheader>
           {
-              demoCategories.map(({label, value}) => (
-                <Link key={value} className={classes.links} to="/">
+              isLoading ? (
+                <Box display="flex" justifyContent="center">
+                  <CircularProgress size="4rem" />
+                </Box>
+              ) : error ? (
+                <p>Error has ocurred</p>
+              ) :
+              genresList.genres.map(({name, id}) => (
+                <Link key={id} className={classes.links} to="/">
                     <ListItem onClick={() => {}} button>
-                        {/* <ListItemIcon>
-                            <img className={classes.genreImage} src={redLogo} alt="Logo" height={30} />
-                        </ListItemIcon> */}
-                        <ListItemText primary={label} />
+                        <ListItemIcon>
+                            <img className={classes.genreImage} src={genreIcons[name.toLowerCase()]} alt="Logo" height={30} />
+                        </ListItemIcon>
+                        <ListItemText primary={name} />
                     </ListItem>
                 </Link>
               ))
